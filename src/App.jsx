161ca6d00990ef628aafa44fc7d04dee6e5f1599
AdnanSakib04 <navidadnan04@gmail.com>
2023-09-14 21:51:16 +0600

    google fonts Inter added

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+     
+      <h1 className=' text-center text-3xl font-bold mt-12'>Course Registration</h1>
+      
+    </>
+  )
+}
+
+export default App
