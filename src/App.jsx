2707bf0a2e635842b84653b533cf9e766f9d0bc1
AdnanSakib04 <navidadnan04@gmail.com>
2023-09-14 22:37:14 +0600

    Courses component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
 
 
+import { useEffect } from 'react';
 import './App.css'
+import Cards from './components/Courses/Courses'
+import { useState } from 'react';
 
 function App() {
-
+  const [courses, setCourses] = useState([]);
+  useEffect(() => {
+    fetch("./course-data.json")
+      .then((res) => res.json())
+      .then((data) => setCourses(data));
+  }, []);
   return (
     <>
 
      
       <h1 className=' text-center text-3xl font-bold mt-12'>Course Registration</h1>
+      <div className=' max-w-[1400px] mx-auto'>
+      <div className='w-3/4'>
+      <Cards courses={courses}></Cards>
+      </div>
+      </div>
+      <div className='w-1/4'>
+
+
+
+      </div>
+      
+      
       
     </>
   )
---
+++ src/components/Courses/Courses.jsx
@@ -0,0 +1,10 @@
+
+const Courses = ({courses}) => {
+    return (
+        <div>
+            <h1>Course: {courses.length}</h1>
+        </div>
+    );
+};
+
+export default Courses;
